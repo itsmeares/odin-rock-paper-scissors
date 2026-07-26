afafcef73d814833d30fb3f1d48aa6975a8a6193
Aras <72440556+itsmeares@users.noreply.github.com>
2026-07-26 06:49:48 +0100

    Add core functions for the game

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello World!")
+function getComputerChoice() {
+    // get a random number between 0 - 1
+    let randomNumber = Math.random();
+
+    if (randomNumber <= 0.33) {
+        // if number is between 0 - 0.33 return rock
+        return "Rock";
+    } else if (randomNumber >= 0.33 && randomNumber <= 0.66) {
+        // else if number is between 0.33 - 0.66 return paper
+        return "Paper";
+    } else {
+        // else return scissors
+        return "Scissors";
+    }
+}
+
+function getHumanChoice() {
+    // prompt the user for an input
+    let humanChoice = prompt("What's your choice?", "");
+}
